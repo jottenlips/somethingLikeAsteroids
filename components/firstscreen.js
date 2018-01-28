c7aceedf6754ed6b51cbad3ed021426685a38906
Robert Spurlin <rts3e@mtmail.mtsu.edu>
2018-01-28 10:15:10 -0600

    Redesign of firstScreen

--- components/firstscreen.js
+++ components/firstscreen.js
@@ -20,22 +20,29 @@ export default class FirstScreen extends React.Component {
                         width: 4,
                         alignItems: 'stretch',
                         transform: [{translate: [-2, 2, -5]}],
-                        backgroundColor: 'red'
+                        backgroundColor: 'grey',
+                        borderWidth: 0.05,
+                        borderColor: "#ffffff"
                     }}>
 
                     <View style={{marginTop: 0.5, marginBottom: 1, height: 0.4}}>
                         <Text style={{fontSize: 0.3, textAlign: 'center', color: "#ffffff"}}>Something Like Asterioids</Text>
                     </View>
 
+                    {/*
+
+                    For another day.. 
+
                     <VrButton onClick={()=>this.props.isMultiplayer(false)} style={{marginTop: 0.5, marginBottom: 0.5}}>
                         <View style={{ height: 0.5, borderWidth: 0.05, borderColor: "#ffffff" }}>
                             <Text style={{fontSize: 0.2, textAlign: 'center', color: "#ffffff"}}>1 Player</Text>
                          </View>
                     </VrButton>
+                    */}
 
-                    <VrButton onClick={()=>this.props.isMultiplayer(true)} style={{marginBottom: 0.5}}>
-                        <View style={{ height: 0.5, borderWidth: 0.05, borderColor: "#ffffff" }}>
-                            <Text style={{fontSize: 0.2, textAlign: 'center', color: "#ffffff"}}>2+ Players</Text>
+                    <VrButton onClick={()=>this.props.isMultiplayer(true)} style={{marginBottom: 0.5, marginLeft: 1, marginRight: 1}}>
+                        <View style={{ height: 0.5, backgroundColor: "#ffffff", alignItems: 'center', justifyContent: 'center' }}>
+                            <Text style={{fontSize: 0.2, textAlign: 'center', color: "#000000"}}>Start the Game</Text>
                         </View>
                     </VrButton>
                         
--- components/game.js
+++ components/game.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { asset, Pano, AmbientLight } from "react-vr";
 import { PropTypes } from 'prop-types';
 import Laser from "../containers/laser-container";
